7e2c233afedcc213e6849cad1f3b2722ec5b385a
Nguyen Ngoc Khanh <khanhnnvn@gmail.com>
2025-08-27 14:14:28 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add your Firebase project configuration here
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "resumerank-ai-1fsl9",
+  "appId": "1:513233036083:web:5d7a208489fd27c264d446",
+  "storageBucket": "resumerank-ai-1fsl9.firebasestorage.app",
+  "apiKey": "AIzaSyDuXKLjjy1EFi5sexwrTebGhTmAoXB9IdQ",
+  "authDomain": "resumerank-ai-1fsl9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "513233036083"
 };
 
 // Initialize Firebase
